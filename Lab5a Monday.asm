95f2441470a438e9fc780510465e7d5c167cf983
Brittany <britt.garcia123@gmail.com>
2017-09-23 15:25:53 -0700

    Update and rename Lab 5 Monday.asm to Lab5a Monday.asm

--- Lab5a Monday.asm
+++ Lab5a Monday.asm
@@ -17,9 +17,9 @@ error2:	 .asciiz "Error cannot be greater than 10\n"
 main:
 #initialize variables
 	li $s0, 0 	#counter
-	li $s4, 1		#int 1
+	li $s4, 1	#int 1
 	li $s7, 10	#int 10
-	li $t9, 4		#int 4
+	li $t9, 4	#int 4
 	la $s1, array 	#array
 	
 #prompt user to figure out how big array will be
@@ -33,7 +33,7 @@ main:
 	add $s3, $v0, $0 	#store user input into $s3
 	
 	bgt $s3, $s7, errortoohigh #compares #s3 to #s7 to see if $s3 > $s7
-	ble $s3, $0, errortoolow #compares $s3 to $0 to see if $s3 <= $0
+	ble $s3, $0, errortoolow   #compares $s3 to $0 to see if $s3 <= $0
 	
 	add $s6, $s3, $0 	#moves array size to $s6
 	j loop
@@ -52,22 +52,20 @@ loop: beq $s0, $s3, block 	#checks to see if user input has filled the array, co
 	syscall
 
 	sw $v0, 0($s1) 	#sw in $vo into RAM at address contained into $s1(array) store word from user into the register of the array
-	add $s1, $s1, 4	#add 4 to the array
-	add $s0, $s0, 1	#add 1 to counter
-	#add $s5, $s5, 1	#add 1 to $s5 to change position of element (Enter number ...)
+	add $s1, $s1, 4		#add 4 to the array
+	add $s0, $s0, 1		#add 1 to counter
 	
 	j loop
-
 block:
 	#overriding the array and counter to reset back to top
 	li $s0, 0 	#counter
 	la $s1, array 	#array
 
 	la $a0, endprompt 	#end prompt says The numbers in the array are: 
-	li $v0, 4 	#load immediate appropriate call code 4(print string) into $v0
+	li $v0, 4 		#load immediate appropriate call code 4(print string) into $v0
 	syscall
 	
-	mult $s6, $t9	#$s6 is array size $t9 is 4
+	mult $s6, $t9		#$s6 is array size $t9 is 4
 	mflo $t0		#arraysize*4 stored in $t0
 	add $s1, $t0, $s1	#adding multiplication to array($s1 = array)
 	
@@ -75,17 +73,17 @@ print: beq $s0, $s3, exit	#$s3 is number of elements in the array as stated by u
 
 	sub $s1, $s1, 4 	#add 4 to the first address of the array, which is now last address of array, moving the pointer to the next address each time
 	
-	lw $a0, 0($s1) 	#load word from $a0 into position 0 of the array($s1)
+	lw $a0, 0($s1) 		#load word from $a0 into position 0 of the array($s1)
 	li $v0, 1		#load immediate value of 1, print int, into $v0
 	syscall
 	
 	#prints a space between numbers
-	la $a0, space 	#load address of space into $a0, space: .asciiz " "
+	la $a0, space 		#load address of space into $a0, space: .asciiz " "
 	li $v0, 4		#load immediate appropriate call code 4(print string) into $v0
 	syscall		
 	
 	
-	sub $s3, $s3, 1	#counter - 1 to get it back to the first position of the array
+	sub $s3, $s3, 1		#counter - 1 to get it back to the first position of the array
 	j print
 
 exit:
